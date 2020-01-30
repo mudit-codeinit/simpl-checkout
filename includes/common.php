748c780ba18c6a253264f9df18f522c09c95de1f
prithvi <prithvi@buzznerd.com>
2020-01-30 14:04:35 +0530

    add Query string

--- includes/common.php
+++ includes/common.php
@@ -2,6 +2,7 @@
 	require __DIR__ . '/../core/helper.php';
 
 	if(checkDevice()==1){
-		header('Location: mobile/index.php');
+		  	$query_string = isset($_SERVER["QUERY_STRING"]) ?  $_SERVER["QUERY_STRING"]:'';
+		header('Location: mobile/index.php?'.$query_string);
 	}
 ?>
